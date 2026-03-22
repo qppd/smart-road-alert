614258a11295bdc697243ba80f901516d165ffc4
Quezon Province Programmer, Developers <quezon.province.pd@gmail.com>
2026-03-22 21:18:12 +0800

    Updated

--- src/sample/gtts/SpeakGTTS.py
+++ src/sample/gtts/SpeakGTTS.py
@@ -2,7 +2,7 @@ from gtts import gTTS
 import os
 
 text = "STOP KAMOTE" 
-tts = gTTS(text=text, lang='en')
+tts = gTTS(text=text, lang='tl')
 
 tts.save("output.mp3")
 os.system("mpg123 output.mp3")        
